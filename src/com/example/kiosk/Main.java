2233ebff859c7f4d9f7885bda986600df5a59858
xaehub <novxae@gmail.com>
2025-03-07 12:14:26 +0900

    feat: 사용자 편의성 향상을 위한 코드 수정 및 추가(Lv.1)

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -16,25 +16,32 @@ public class Main {
         System.out.println("     5.바닐라라떼           5.5/6.0");
         System.out.println("     0.종료                       ");
 
+        System.out.println("원하시는 메뉴를 선택해 주세요: ");
+
         while(true){
-            System.out.println("원하시는 메뉴를 선택해 주세요: ");
             int a = sc.nextInt();
 
              if(a == 1) {
                  System.out.println("에스프레소에 뜨거운 물을 희석하여 만든 커피 음료");
+                 System.out.println("추가 선택: ");
              } else if(a == 2) {
                  System.out.println("볶은 커피를 가늘게 분쇄한 뒤, 고온의 물을 가해 짧은 시간에 추출한 진한 커피");
+                 System.out.println("추가 선택: ");
              } else if(a == 3) {
                  System.out.println("에스프레소를 베이스로 한 우유를 넣은 커피 음료");
+                 System.out.println("추가 선택: ");
              } else if(a == 4) {
                  System.out.println("에스프레소, 우유, 초콜릿 시럽을 넣어 만든 커피 음료");
+                 System.out.println("추가 선택: ");
              } else if(a == 5) {
                  System.out.println("에스프레소에 우유와 바닐라시럽을 섞어 만든 커피");
+                 System.out.println("추가 선택: ");
              } else if(a == 0) {
                  System.out.println("키오스크 종료");
                  break;
              } else {
                  System.out.println("잘못된 번호를 입력하셨습니다.");
+                 System.out.println("추가 선택: ");
              }
         }
     }
