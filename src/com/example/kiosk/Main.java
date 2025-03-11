ab901487fce00d7196044fae5029f7cc0dfbc48b
xaehub <novxae@gmail.com>
2025-03-11 10:48:08 +0900

    feat: MenuItem 클래스 생성하여 개별 음식 항목 관리 기능 구현

--- src/com/example/kiosk/Main.java
+++ src/com/example/kiosk/Main.java
@@ -6,15 +6,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("     ----------Nov Cafe----------");
-        System.out.println("     ------------Menu------------");
-        System.out.println("     ---Coffee---         hot/ice");
-        System.out.println("     1.아메리카노           4.5/5.0");
-        System.out.println("     2.에스프레소           5.0/5.5");
-        System.out.println("     3.카푸치노             5.0/6.0");
-        System.out.println("     4.카페모카             5.5/6.0");
-        System.out.println("     5.바닐라라떼           5.5/6.0");
-        System.out.println("     0.종료                       ");
+        System.out.println("--------------------Nov Cafe--------------------");
+        System.out.println("----------------------Menu----------------------");
+
+        MenuItem americano = new MenuItem("1. 아메리카노", 4.5, "에스프레소에 뜨거운 물을 희석하여 만든 커피 음료");
+        MenuItem espresso = new MenuItem("2. 에스프레소", 5.0, "볶은 커피를 가늘게 분쇄한 뒤, 고온의 물을 가해 짧은 시간에 추출한 진한 커피");
+        MenuItem cappuccino = new MenuItem("3. 카푸치노", 5.0, "에스프레소를 베이스로 한 우유를 넣은 커피 음료");
+        MenuItem mocha = new MenuItem("4. 카페모카", 5.5, "에스프레소, 우유, 초콜릿 시럽을 넣어 만든 커피 음료");
+        MenuItem vanilla = new MenuItem("5. 바닐라 라떼", 5.5, "에스프레소에 우유와 바닐라시럽을 섞어 만든 커피");
+
+
+        americano.cafeMenu();
+        espresso.cafeMenu();
+        cappuccino.cafeMenu();
+        mocha.cafeMenu();
+        vanilla.cafeMenu();
 
         System.out.println("원하시는 메뉴를 선택해 주세요: ");
 
@@ -22,26 +28,27 @@ public class Main {
             int a = sc.nextInt();
 
              if(a == 1) {
-                 System.out.println("에스프레소에 뜨거운 물을 희석하여 만든 커피 음료");
-                 System.out.println("추가 선택: ");
+                 System.out.println("아메리카노");
+                 System.out.println("추가 선택(종료는 0): ");
              } else if(a == 2) {
-                 System.out.println("볶은 커피를 가늘게 분쇄한 뒤, 고온의 물을 가해 짧은 시간에 추출한 진한 커피");
-                 System.out.println("추가 선택: ");
+                 System.out.println("에스프레소");
+                 System.out.println("추가 선택(종료는 0): ");
              } else if(a == 3) {
-                 System.out.println("에스프레소를 베이스로 한 우유를 넣은 커피 음료");
-                 System.out.println("추가 선택: ");
+                 System.out.println("카푸치노");
+                 System.out.println("추가 선택(종료는 0): ");
              } else if(a == 4) {
-                 System.out.println("에스프레소, 우유, 초콜릿 시럽을 넣어 만든 커피 음료");
-                 System.out.println("추가 선택: ");
+                 System.out.println("카페모카");
+                 System.out.println("추가 선택(종료는 0): ");
              } else if(a == 5) {
-                 System.out.println("에스프레소에 우유와 바닐라시럽을 섞어 만든 커피");
-                 System.out.println("추가 선택: ");
+                 System.out.println("바닐라 라떼");
+                 System.out.println("추가 선택(종료는 0): ");
              } else if(a == 0) {
                  System.out.println("키오스크 종료");
                  break;
+
              } else {
                  System.out.println("잘못된 번호를 입력하셨습니다.");
-                 System.out.println("추가 선택: ");
+                 System.out.println("추가 선택(종료는 0): ");
              }
         }
     }
--- src/com/example/kiosk/MenuItem.java
+++ src/com/example/kiosk/MenuItem.java
@@ -1,4 +1,24 @@
 package com.example.kiosk;
 
 public class MenuItem {
+    // 속성
+    private String name;
+    private double price;
+    private String menuInfo;
+
+    // 생성자
+    public MenuItem(String name, double price, String menuInfo) {
+        this.name = name;
+        this.price = price;
+        this.menuInfo = menuInfo;
+    }
+
+    // 기능
+
+    public void cafeMenu() {
+        System.out.println(name + "   |   " + price + "   |   " + menuInfo);
+    }
+
+
+
 }
